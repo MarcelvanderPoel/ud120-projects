94529e868594bee8e122dddf5bf2e3cc3e9c7997
Marcel van der Poel <marcel@vanderpoelonline.nl>
2017-05-29 22:06:16 +0200

    Final version choose your own algorithm

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -3,6 +3,9 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -31,12 +34,12 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+clf = RandomForestClassifier(n_estimators=10)
+clf.fit(features_train, labels_train)
+pred=clf.predict(features_test)
 
-
-
-
-
-
+accuracy=accuracy_score(labels_test, pred)
+print "accuracy: ", accuracy
 
 try:
     prettyPicture(clf, features_test, labels_test)
--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -13,6 +13,7 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -26,8 +27,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### my code goes here ###
 
-clf=tree.DecisionTreeClassifier()
-clf.fit(features_train, labels_train)
+clf_2=tree.DecisionTreeClassifier(min_samples_split=2)
+clf_2.fit(features_train, labels_train)
+pred_2=clf_2.predict(features_test)
+
+acc_min_samples_split_2=accuracy_score(labels_test, pred_2)
+print "accuracy 2: ", acc_min_samples_split_2
+
+clf_50=tree.DecisionTreeClassifier(min_samples_split=50)
+clf_50.fit(features_train, labels_train)
+pred_50=clf_50.predict(features_test)
+
+acc_min_samples_split_50=accuracy_score(labels_test, pred_50)
+print "accuracy 50: ", acc_min_samples_split_50
+
 
 #########################################################
 
