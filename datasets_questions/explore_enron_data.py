93ac6213b3b13941a2f1542252df9ab463661994
Marcel van der Poel <marcel@vanderpoelonline.nl>
2017-06-11 22:12:57 +0200

    Examining enron data set

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -15,8 +15,25 @@
     
 """
 
+import sys
 import pickle
+import numpy
+
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+
+print enron_data["SKILLING JEFFREY K"]["total_payments"]
+print enron_data["LAY KENNETH L"]["total_payments"]
+print enron_data["FASTOW ANDREW S"]
+
+d = dict((k, v) for k, v in enron_data.items() if v["total_payments"] == "NaN")
+
+print len(d)
+
+feature_list = ["total_payments", "poi"]
+data_array = featureFormat(enron_data, feature_list, remove_NaN=True, remove_all_zeroes=True, remove_any_zeroes=True, sort_keys = False )
+print data_array
 
 
